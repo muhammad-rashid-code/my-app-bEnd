a3dd633cf9f8695412921811203da7d1fc72db93
Muhammad Rashid <m.rashid212823@gmail.com>
2025-02-03 02:22:48 +0500

    good to go

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT;
+const db = process.env.MONGO_URI;
+
+app.use(express.json());
+app.use(morgan("common"));
+app.use(cors("*"));
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log(" ===== db conn =====");
+  })
+  .catch((e) => {
+    console.log(console.log(" ===== db fail =====\n" + e));
+  });
+
+app.listen(port, () => {
+  console.log("Server is Runnig at PORT", port);
+});
+ 
